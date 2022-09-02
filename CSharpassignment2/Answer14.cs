732fa295dc443bc5183ae0966f624cacedb9b3a7
Thulunga Basumatary <thulunga.basumatary@soti.net>
2022-09-02 18:35:14 +0530

    answer 14 15 are added

--- CSharpassignment2/Answer14.cs
+++ CSharpassignment2/Answer14.cs
@@ -4,15 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//14.Write a program in C# Sharp to display the n terms of odd natural number and their sum. 
+//Test Data
+//Input number of terms : 10
+//Expected Output :
+//The odd numbers are :1 3 5 7 9 11 13 15 17 19
+//The Sum of odd Natural Number upto 10 terms : 100
+
+
 namespace CSharpassignment2
 {
     internal class Answer14
     {
         static void Main14()
         {
-            System.Console.WriteLine("Thulunga BAsumatry11");
+            int i, n, sum = 0;
+            Console.Write("\n\n");
+            Console.Write("\n\n");
 
-            Console.ReadKey();
+            Console.Write("Input number of terms : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nThe odd numbers are :");
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write("{0} ", 2 * i - 1);
+                sum += 2 * i - 1;
+            }
+            Console.Write("\nThe Sum of odd Natural Number upto {0} terms : {1} \n", n, sum);
+        
+        Console.ReadKey();
         }
     }
 }
--- CSharpassignment2/Answer15.cs
+++ CSharpassignment2/Answer15.cs
@@ -4,13 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//15.Write a program in C# Sharp to display the pattern like right angle triangle using an asterisk. 
+//The pattern like :
+
+//*
+//**
+//***
+//****
+
 namespace CSharpassignment2
 {
     internal class Answer15
     {
         static void Main15()
         {
-            System.Console.WriteLine("Thulunga BAsumatry11");
+            int i, j, rows;
+            Console.Write("\n\n");
+            Console.Write("\n\n");
+
+            Console.Write("Input number of rows : ");
+            rows = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= rows; i++)
+            {
+                for (j = 1; j <= i; j++)
+                    Console.Write("*");
+                Console.Write("\n");
+            }
+
+
 
             Console.ReadKey();
         }
